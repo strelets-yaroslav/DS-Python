695fd7025b30b1259fc0ae7a03db21ceb2527ad8
Yaroslav Strelets <strelets_yaroslav@yahoo.com>
2021-07-17 12:29:51 +0300

    task #2

--- lesson_1/lesson_1.py
+++ lesson_1/lesson_1.py
@@ -14,13 +14,38 @@ def print_task_description(task_number):
 пользователя некоторые числа и строки и сохраните в переменные, затем выведите на экран.
 '''
 
-print_task_description(1)
+# print_task_description(1)
+#
+# first_var = "I'm first var"
+# second_var = 3
+# third_var = [1, 2, 'fff']
+# print(f"first vars: string - {first_var}; number - {second_var}; list - {third_var}")
+#
+# user_first_var = input('Enter some var: ')
+# user_second_var = input('And a bit more...: ')
+# print(f'You entered: {user_first_var} and {user_second_var}')
 
-first_var = "I'm first var"
-second_var = 3
-third_var = [1, 2, 'fff']
-print(f'first vars: string - {first_var}; number - {second_var}; list - {third_var}')
 
-user_first_var = input('Enter some var: ')
-user_second_var = input('And a bit more...: ')
-print(f'You entered: {user_first_var} and {user_second_var}')
+'''
+2. Пользователь вводит время в секундах. Переведите время в часы, минуты, секунды и
+выведите в формате чч:мм:сс. Используйте форматирование строк.
+'''
+
+print_task_description(2)
+
+raw_seconds = 0
+while True:
+    raw_seconds = input('Enter time in seconds: ')
+    if raw_seconds.isdigit():
+        raw_seconds = int(raw_seconds)
+        break
+    print('Digit is required...', end=' ')
+
+seconds = raw_seconds % 60
+minutes = (raw_seconds % 3600) // 60
+raw_hours = raw_seconds // 3600
+print(f'{raw_hours:02d}:{minutes:02d}:{seconds:02d}')
+if raw_hours > 23:
+    hours = raw_hours % 24
+    days = raw_hours // 24
+    print(f'{days} days {hours:02d}:{minutes:02d}:{seconds:02d}')
