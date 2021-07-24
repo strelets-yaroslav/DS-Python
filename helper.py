ab9ad138da5439c011d6a3c93b92685e6ac425b6
Yaroslav Strelets <strelets_yaroslav@yahoo.com>
2021-07-24 11:46:17 +0300

    lesson3 - helper.py

--- helper.py
+++ helper.py
@@ -17,13 +17,37 @@ def print_incorrect():
     print('Incorrect value...', end=' ')
 
 
-def get_number(prompt='Enter number', non_zero=False):
-    user_number = 0
+def get_number(prompt='Enter number', number_type=int, is_pos=False, is_neg=False, non_zero=False):
+    """
+    takes user input and tries to convert it to the type pointed to by the number_type parameter,
+    additionally checking several possible conditions.
+
+    :param prompt: prompt for user input (default - 'Enter number') - str
+    :param number_type: type for number (default - int) - type
+    :param is_pos: is number positive (default - False) - boolean
+    :param is_neg: is number negative (default - False) - boolean
+    :param non_zero: is number not equal zero (default - False) - boolean
+    :return: number - type of number_type param
+    """
+
     while True:
         user_number = input(prompt + ': ')
-        if user_number.isdigit():
-            user_number = int(user_number)
-            if not non_zero or (non_zero and user_number != 0):
+        try:
+            user_number = number_type(user_number)
+        except ValueError:
+            pass
+        else:
+            if not (is_neg or is_pos or non_zero):
                 break
+
+            if is_pos and user_number > 0:
+                break
+
+            if is_neg and user_number < 0:
+                break
+
+            if non_zero and user_number != 0:
+                break
+
         print_incorrect()
     return user_number
